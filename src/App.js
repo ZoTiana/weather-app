d8b7420f9d2046897dc810d7b915954723fd4c75
Zo Tiana ANDRIAMIFIDY <zotianaandriamifidy@gmail.com>
2024-08-27 14:18:57 +0300

    Change City

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Antananarivo" />
+        <Weather defaultCity="Tokyo" />
         <footer>
           This project was coded by Zo Tiana and is{" "}
           <a
