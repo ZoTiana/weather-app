40b61880b10a6e2e650802810056b2b2c6cfd138
Zo Tiana ANDRIAMIFIDY <zotianaandriamifidy@gmail.com>
2024-09-03 17:05:54 +0300

    Build forecast for 1 day

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,16 +1,32 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}`;
+  }
+
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = days[date.getDay()];
+    return `${day}`;
+  }
   return (
     <div>
-      <div className="WeatherForecast-day">{props.data.time}</div>
+      <div className="WeatherForecast-day">{day()}</div>
       <WeatherIcon code={props.data.condition.icon} />
       <div className="WeatherForecast-temperature">
         <span className="WeatherForecast-temperature-max">
-          {props.data.temperature.maximum}°
+          {maxTemperature()}°
         </span>
         <span className="WeatherForecast-temperature-min">
-          {props.data.temperature.minimum}°
+          {minTemperature()}°
         </span>
       </div>
     </div>
