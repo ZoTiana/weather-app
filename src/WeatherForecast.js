74afb24f0b8327ed2db9f05eb3c44b66912c1a22
Zo Tiana ANDRIAMIFIDY <zotianaandriamifidy@gmail.com>
2024-09-03 13:16:28 +0300

    get axios for the WeatherForecast component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,16 @@
 import React from "react";
+import axios from "axios";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data.daily[0].temperature.minimum);
+  }
+
+  let apiKey = "d73d01c748aatfb2o0bcd43d19416a3e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=Lisbon&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
